995557b9a3f0175ec97646b1167c84f39c33e27f
guoshencheng <648772021@qq.com>
2017-05-21 11:32:11 +0800

    add dist

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var db = require('./db');
 
 /* GET home page. */
 index.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+  var env = process.env.NODE_ENV || "develop";
+  res.render('home', { env });
 });
 
 module.exports = {
---
+++ webpack.config.product.js
@@ -0,0 +1,58 @@
+var webpack = require('webpack')
+var path = require('path')
+var fs = require('fs')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: [path.resolve(__dirname, './react/index.jsx')]
+  },
+  output: {
+      path: path.resolve(__dirname, './public/dist'),
+      filename: "[name].js",
+      publicPath: "/dist/"
+  },
+  externals:{
+    react:'React',
+    'react-dom':'ReactDOM',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.scss$/, 
+        loader: ExtractTextPlugin.extract("style", "css!sass") 
+      },
+      {
+        test: /\.md$/,
+        loaders: ['raw-loader']
+      }
+    ]
+  },
+  plugins:[
+    new ExtractTextPlugin("[name].css"),
+    new webpack.DefinePlugin({
+      env:{
+        isDevelopment:true,
+        BROWSER_ENV: true
+      },
+      __DEV__:true,
+      __DEBUG__:true,
+    })
+  ],
+}
