78d318b801536d68dd19fa51797d47a6dceba445
guoshencheng <648772021@qq.com>
2017-08-03 00:49:14 +0800

    add views

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', env: env, hash: resourceHash.hash });
 });
 
+router.get('/mock', function(req, res, next) {
+  res.render('mock', { title: 'Express', env: env, hash: resourceHash.hash });
+});
+
+router.get('/flow', function(req, res, next) {
+  res.render('flow', { title: 'Express', env: env, hash: resourceHash.hash });
+});
+
+router.get('/blogs', function(req, res, next) {
+  res.render('blog/index', { title: 'Express', env: env, hash: resourceHash.hash });
+});
+
 module.exports = router;
